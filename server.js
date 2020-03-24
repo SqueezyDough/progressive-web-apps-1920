5ce73e0e1a2645187e0ee6e5df273c555d27948c
SqueezyDough <lvanbiljouw@hotmail.com>
2020-03-24 11:49:27 +0100

    Add compression

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const router = require('./routes/index.routes')
-const minifyHTML = require('express-minify-html-2');
+const minifyHTML = require('express-minify-html-2')
+const compression = require('compression')
 const bodyParser = require('body-parser')
+
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -16,6 +18,7 @@ app
     .use("/", express.static(path.join(__dirname, "public")))
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
+    .use(compression({ filter: shouldCompress }))
     .set('view engine', 'hbs')
     .engine('hbs', exphbs({
         extname: '.hbs',
@@ -36,3 +39,13 @@ app
     }))
     .use('/', router)
     .listen(port, () => console.log(`Listening on port ${port}!`))
+
+function shouldCompress (req, res) {
+    if (req.headers['x-no-compression']) {
+        // don't compress responses with this request header
+        return false
+    }
+
+    // fallback to standard filter function
+    return compression.filter(req, res)
+}
